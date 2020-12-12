d250769ea32ca70c09071d332c2e0b845336c444
PavelChugreev <chugun92@gmail.com>
2020-12-12 03:37:02 +0300

    get start condition from outer file by json-server

--- animation.js
+++ animation.js
@@ -1,77 +1,96 @@
-const field = document.querySelector('canvas');
-const ctx = field.getContext('2d');
-
-const cellSize = 10;
-field.width = 600;
-field.height = 600;
-
-const rows = field.height / cellSize;
-const cols = field.width / cellSize;
-
-function createCells() {
-    const rowsArr = new Array;
-    for (let i = 0; i < rows; i++) {
-        rowsArr.push(new Array);
-        for (let j = 0; j < cols; j++) {
-            rowsArr[i].push(Math.floor(Math.random() * 2));
-        }
+const getStartCondition = async (url) => {
+    const result = await fetch(url);
+    if (!result.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${result.status}`);
     }
-    return rowsArr
-}
+    return await result.json();
+};
 
-let grid = createCells();
+let data;
+getStartCondition("http://localhost:3000/startCondition")
+    .then(res => data = res)
+    .then(() => {
 
-function updateGrid(grid) {
-    const newGrid = grid.map(arr => [...arr]);
+        const field = document.querySelector('canvas');
+        const ctx = field.getContext('2d');
 
-    for (let col = 0; col < grid.length; col++) {
-        for (let row = 0; row < grid[col].length; row++) { 
-            const cell = grid[col][row]; 
-            let neighbours = 0;
+        // const cellSize = 10;
+        field.width = 600;
+        field.height = 600;
 
-            for (let i = -1; i < 2; i++) { 
-                for (let j = -1; j < 2; j++) { 
-                    if (i === 0 && j === 0) {
-                        continue;
+        let cellSize = field.width / data.length;
+
+        const rows = field.height / cellSize;
+        const cols = field.width / cellSize;
+
+        function createCells() {
+            const rowsArr = new Array;
+            for (let i = 0; i < rows; i++) {
+                rowsArr.push(new Array);
+                for (let j = 0; j < cols; j++) {
+                    // rowsArr[i].push(Math.floor(Math.random() * 2));
+                    rowsArr[i].push(data[i][j]);
+                }
+            }
+            return rowsArr
+        }
+
+        let grid = createCells();
+
+        function updateGrid(grid) {
+            const newGrid = grid.map(arr => [...arr]);
+
+            for (let col = 0; col < grid.length; col++) {
+                for (let row = 0; row < grid[col].length; row++) {
+                    const cell = grid[col][row];
+                    let neighbours = 0;
+
+                    for (let i = -1; i < 2; i++) {
+                        for (let j = -1; j < 2; j++) {
+                            if (i === 0 && j === 0) {
+                                continue;
+                            }
+
+                            const cellRow = row + j;
+                            const cellCol = col + i;
+                            if (cellRow >= 0 && cellCol >= 0 && cellCol < cols && cellRow < rows) {
+                                const currentNeighbour = grid[col + i][row + j];
+                                neighbours += currentNeighbour;
+                            }
+                        }
                     }
 
-                    const cellRow = row + j;
-                    const cellCol = col + i;
-                    if (cellRow >= 0 && cellCol >= 0 && cellCol < cols && cellRow < rows) {
-                        const currentNeighbour = grid[col + i][row + j];
-                        neighbours += currentNeighbour;
+                    if (cell === 1) {
+                        if (neighbours < 2 || neighbours > 3) {
+                            newGrid[col][row] = 0;
+                        }
+                    } else if (cell === 0 && neighbours === 3) {
+                        newGrid[col][row] = 1;
                     }
                 }
             }
+            return newGrid;
+        }
 
-            if (cell === 1) {
-                if(neighbours < 2 || neighbours > 3){
-                    newGrid[col][row] = 0;
-                } 
-             } else if (cell === 0 && neighbours === 3) {
-                newGrid[col][row] = 1;
+        function render(grid) {
+            for (let i = 0; i < grid.length; i++) {
+                for (let j = 0; j < grid[i].length; j++) {
+                    const cell = grid[i][j];
+
+                    ctx.beginPath();
+                    ctx.rect(i * cellSize, j * cellSize, cellSize, cellSize);
+                    ctx.fillStyle = cell ? 'black' : 'white';
+                    ctx.fill();
+                    ctx.stroke();
+                }
             }
         }
-    }
-    return newGrid;
-}
 
-function render(grid) {
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[i].length; j++) {
-            const cell = grid[i][j];
+        setInterval(() => {
+            grid = updateGrid(grid);
+            render(grid);
+            console.log(grid)
+        }, 2000)
 
-            ctx.beginPath();
-            ctx.rect(i * cellSize, j * cellSize, cellSize, cellSize);
-            ctx.fillStyle = cell ? 'black' : 'white';
-            ctx.fill();
-            ctx.stroke();
-        }
-    }
-}
+    })
 
-setInterval(() => {
-    grid = updateGrid(grid);
-    render(grid);
-    console.log(grid)
-}, 500)
--- index.js
+++ index.js
@@ -1,55 +1,80 @@
-const cols = 5;
-const rows = 5;
-
-function createCells() {
-    const arr = new Array;
-    for(let i = 0; i < cols; i++){
-        arr.push(new Array);
-        for(let j = 0; j < rows; j++){
-            arr[i].push(Math.floor(Math.random() * 2));
-        }
+const getStartCondition = async (url) => {
+    const result = await fetch(url);
+    if (!result.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${result.status}`);
     }
-    return arr
-}
+    return await result.json();
+};
 
-let grid = createCells();
+let data;
+getStartCondition("http://localhost:3000/startCondition")
+    .then(res => data = res)
+    .then(() => {
+        let rows, cols, count;
+        
+        data.forEach((item, i) => {
+            if (i !== 0) {
+                if (item.length == data[i - 1].length) {
+                    count = item.length
+                }
+            }
+        })
 
-function updateGrid(grid) {
-    const newGrid = grid.map(arr => [...arr]);
+        rows = data.length;
+        cols = count;
 
-    for (let col = 0; col < grid.length; col++) {
-        for (let row = 0; row < grid[col].length; row++) { 
-            const cell = grid[col][row]; 
-            let neighbours = 0;
+        setInterval(() => {
+            grid = updateGrid(grid);
+            console.log(grid)
+        }, 1000)
+        // const cols = 5;
+        // const rows = 5;
 
-            for (let i = -1; i < 2; i++) { 
-                for (let j = -1; j < 2; j++) { 
-                    if (i === 0 && j === 0) {
-                        continue;
+        function createCells() {
+            const arr = new Array;
+            for (let i = 0; i < cols; i++) {
+                arr.push(new Array);
+                for (let j = 0; j < rows; j++) {
+                    arr[i].push(Math.floor(Math.random() * 2));
+                }
+            }
+            return arr
+        }
+
+        let grid = createCells();
+
+        function updateGrid(grid) {
+            const newGrid = grid.map(arr => [...arr]);
+
+            for (let col = 0; col < grid.length; col++) {
+                for (let row = 0; row < grid[col].length; row++) {
+                    const cell = grid[col][row];
+                    let neighbours = 0;
+
+                    for (let i = -1; i < 2; i++) {
+                        for (let j = -1; j < 2; j++) {
+                            if (i === 0 && j === 0) {
+                                continue;
+                            }
+
+                            const cellRow = row + j;
+                            const cellCol = col + i;
+                            if (cellRow >= 0 && cellCol >= 0 && cellCol < cols && cellRow < rows) {
+                                const currentNeighbour = grid[col + i][row + j];
+                                neighbours += currentNeighbour;
+                            }
+                        }
                     }
 
-                    const cellRow = row + j;
-                    const cellCol = col + i;
-                    if (cellRow >= 0 && cellCol >= 0 && cellCol < cols && cellRow < rows) {
-                        const currentNeighbour = grid[col + i][row + j];
-                        neighbours += currentNeighbour;
+                    if (cell === 1) {
+                        if (neighbours < 2 || neighbours > 3) {
+                            newGrid[col][row] = 0;
+                        }
+                    } else if (cell === 0 && neighbours === 3) {
+                        newGrid[col][row] = 1;
                     }
                 }
             }
-
-            if (cell === 1) {
-                if(neighbours < 2 || neighbours > 3){
-                    newGrid[col][row] = 0;
-                } 
-             } else if (cell === 0 && neighbours === 3) {
-                newGrid[col][row] = 1;
-            }
+            return newGrid;
         }
-    }
-    return newGrid;
-}
-
-setInterval(() => {
-    grid = updateGrid(grid);
-    console.log(grid)
-}, 1000)
+    });
